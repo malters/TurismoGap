da617e97bfe7355173928b7c6ceb0fdc24311912
jose.mauel <malters@gmail.com>
2013-04-14 10:38:53 +0200

    HEAD

---
+++ TurismoGap/src/com/turismo/MyPhoneGapActivity.java
@@ -0,0 +1,13 @@
+package com.turismo;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class MyPhoneGapActivity extends DroidGap {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setIntegerProperty("loadUrlTimeoutValue", 60000);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
